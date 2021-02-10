08e1e9ddd5bd5046fa2a81404334574c422afff0
Sander <sander@recras.nl>
2021-02-10 15:44:19 +0100

    Tested on 5.7 beta 2

--- readme.txt
+++ readme.txt
@@ -3,7 +3,7 @@ Contributors: zanderz
 Tags: recras, recreation, reservation
 Requires at least: 5.2
 Requires PHP: 5.6.0
-Tested up to: 5.6
+Tested up to: 5.7
 Stable tag: 4.1.5
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
--- recras-wordpress-plugin.php
+++ recras-wordpress-plugin.php
@@ -21,19 +21,5 @@ if (!function_exists('add_action')) {
     die('You cannot run this file directly.');
 }
 
-if (version_compare(phpversion(), '5.6', '<')) {
-    function phpVersionTooOld() {
-        echo '<div class="error">';
-        echo '<h2>Recras WordPress plugin</h2>';
-        echo '<p>';
-        echo 'The Recras WordPress plugin requires PHP 5.6 or higher but your server is running PHP version ' . phpversion() . '. The plugin will not be activated.<br>';
-        echo 'All PHP versions before 7.3.0 have reached "End of Life" and no longer receive bugfixes or security updates. ';
-        echo 'The official information on how to update and why can be found on <a href="https://www.php.net/supported-versions.php" target="_blank"><strong>the PHP website</strong></a>.';
-        echo '</p></div>';
-    }
-    add_action('admin_notices', 'phpVersionTooOld');
-    return;
-}
-
 require_once('vendor/autoload.php');
 $recrasPlugin = new \Recras\Plugin;
