549e7d3cd3f9bc2e6f25cde01b335c461d4b4415
Sander <sander@recras.nl>
2021-08-17 09:16:53 +0200

    The "Term for number of vouchers" is now used during voucher sales

--- src/Plugin.php
+++ src/Plugin.php
@@ -3,7 +3,7 @@ namespace Recras;
 
 class Plugin
 {
-    const LIBRARY_VERSION = '1.8.3';
+    const LIBRARY_VERSION = '1.9.0';
     const TEXT_DOMAIN = 'recras';
 
     const SHORTCODE_ONLINE_BOOKING = 'recras-booking';
