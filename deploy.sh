28faa70aeee1a75352035138974d62c41e4b53e4
Sander de Jong <sander@recras.nl>
2022-03-03 11:59:03 +0100

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,9 @@
 #! /bin/bash
 # See https://github.com/GaryJones/wordpress-plugin-svn-deploy for instructions and credits.
 
+echo
+echo "WordPress Plugin SVN Deploy v4.1.0"
+echo
 
 # Set up some default values. Feel free to change these in your own script
 PLUGINSLUG="recras"
@@ -10,15 +13,19 @@ MAINFILE="recras-wordpress-plugin.php"
 SVNPATH="/tmp/$PLUGINSLUG"
 SVNURL="https://plugins.svn.wordpress.org/$PLUGINSLUG"
 SVNUSER="ZanderZ"
+ASSETSDIR=".wordpress-org"
 
-echo
-echo "WordPress Plugin SVN Deploy v3.0.0"
-echo
+# Check if SVN assets directory exists.
+if [ ! -d "$PLUGINDIR/$ASSETSDIR" ]; then
+	echo "SVN assets directory $PLUGINDIR/$ASSETSDIR not found."
+	echo "This is not fatal but you may not have intended results."
+	echo
+fi
 
 # Check main plugin file exists.
 if [ ! -f "$PLUGINDIR/$MAINFILE" ]; then
-  echo "Plugin file $PLUGINDIR/$MAINFILE not found. Aborting."
-  exit 1;
+	echo "Plugin file $PLUGINDIR/$MAINFILE not found. Aborting."
+	exit 1
 fi
 
 echo "Checking version in main plugin file matches version in readme.txt file..."
@@ -34,7 +41,7 @@ if [ "$READMEVERSION" = "trunk" ]; then
 	echo "Version in readme.txt & $MAINFILE don't match, but Stable tag is trunk. Let's continue..."
 elif [ "$PLUGINVERSION" != "$READMEVERSION" ]; then
 	echo "Version in readme.txt & $MAINFILE don't match. Exiting...."
-	exit 1;
+	exit 1
 elif [ "$PLUGINVERSION" = "$READMEVERSION" ]; then
 	echo "Versions match in readme.txt and $MAINFILE. Let's continue..."
 fi
@@ -54,12 +61,11 @@ cd $PLUGINDIR
 
 # Check for git tag (may need to allow for leading "v"?)
 # if git show-ref --tags --quiet --verify -- "refs/tags/v$PLUGINVERSION"
-if git show-ref --tags --quiet --verify -- "refs/tags/v$PLUGINVERSION"
-	then
-		echo "Git tag v$PLUGINVERSION does exist. Let's continue..."
-	else
-		echo "v$PLUGINVERSION does not exist as a git tag. Aborting.";
-		exit 1;
+if git show-ref --tags --quiet --verify -- "refs/tags/v$PLUGINVERSION"; then
+	echo "Git tag v$PLUGINVERSION does exist. Let's continue..."
+else
+	echo "v$PLUGINVERSION does not exist as a git tag. Aborting."
+	exit 1
 fi
 
 echo
@@ -67,52 +73,62 @@ echo
 echo "Creating local copy of SVN repo trunk..."
 svn checkout $SVNURL $SVNPATH --depth immediates
 svn update --quiet $SVNPATH/trunk --set-depth infinity
+svn update --quiet $SVNPATH/tags/$PLUGINVERSION --set-depth infinity
 
 echo "Ignoring GitHub specific files"
-svn propset svn:ignore "README.md
+# Use local .svnignore if present
+if [ -f ".svnignore" ]; then
+	echo "Using local .svnignore"
+	SVNIGNORE=$(<.svnignore)
+else
+	echo "Using default .svnignore"
+	SVNIGNORE="README.md
 Thumbs.db
-.github/*
+.github
 .git
 .gitattributes
-.gitignore" "$SVNPATH/trunk/"
+.gitignore
+composer.lock"
+fi
+
+svn propset svn:ignore \""$SVNIGNORE"\" "$SVNPATH/trunk/"
 
 echo "Exporting the HEAD of master from git to the trunk of SVN"
 git checkout-index -a -f --prefix=$SVNPATH/trunk/
 
 # If submodule exist, recursively check out their indexes
-if [ -f ".gitmodules" ]
-	then
-		echo "Exporting the HEAD of each submodule from git to the trunk of SVN"
-		git submodule init
-		git submodule update
-		git config -f .gitmodules --get-regexp '^submodule\..*\.path$' |
-			while read path_key path
-			do
-				#url_key=$(echo $path_key | sed 's/\.path/.url/')
-				#url=$(git config -f .gitmodules --get "$url_key")
-				#git submodule add $url $path
-				echo "This is the submodule path: $path"
-				echo "The following line is the command to checkout the submodule."
-				echo "git submodule foreach --recursive 'git checkout-index -a -f --prefix=$SVNPATH/trunk/$path/'"
-				git submodule foreach --recursive 'git checkout-index -a -f --prefix=$SVNPATH/trunk/$path/'
-			done
+if [ -f ".gitmodules" ]; then
+	echo "Exporting the HEAD of each submodule from git to the trunk of SVN"
+	git submodule init
+	git submodule update
+	git config -f .gitmodules --get-regexp '^submodule\..*\.path$' |
+		while read path_key path; do
+			#url_key=$(echo $path_key | sed 's/\.path/.url/')
+			#url=$(git config -f .gitmodules --get "$url_key")
+			#git submodule add $url $path
+			echo "This is the submodule path: $path"
+			echo "The following line is the command to checkout the submodule."
+			echo "git submodule foreach --recursive 'git checkout-index -a -f --prefix=$SVNPATH/trunk/$path/'"
+			git submodule foreach --recursive 'git checkout-index -a -f --prefix=$SVNPATH/trunk/$path/'
+		done
 fi
 
 echo
 
-# Support for the /assets folder on the .org repo.
+# Support for the /assets folder on the .org repo, locally this will be /.wordpress-org
 echo "Moving assets."
 # Make the directory if it doesn't already exist
 mkdir -p $SVNPATH/assets/
-mv $SVNPATH/trunk/assets/* $SVNPATH/assets/
+mv $SVNPATH/trunk/.wordpress-org/* $SVNPATH/assets/
 svn add --force $SVNPATH/assets/
-svn delete --force $SVNPATH/trunk/assets
 
 echo
 
 echo "Changing directory to SVN and committing to trunk."
 cd $SVNPATH/trunk/
 # Delete all files that should not now be added.
+# Use $SVNIGNORE for `rm -rf`. Setting propset svn:ignore seems flaky.
+echo "$SVNIGNORE" | awk '{print $0}' | xargs rm -rf
 svn status | grep -v "^.[ \t]*\..*" | grep "^\!" | awk '{print $2"@"}' | xargs svn del
 # Add all new files that are not set to be ignored
 svn status | grep -v "^.[ \t]*\..*" | grep "^?" | awk '{print $2"@"}' | xargs svn add
@@ -134,9 +150,14 @@ echo
 
 echo "Creating new SVN tag and committing it."
 cd $SVNPATH
+# If current tag not empty then update readme.txt
+if [ -n "$(ls -A tags/$PLUGINVERSION 2>/dev/null)" ]; then
+	echo "Updating readme.txt to tag $PLUGINVERSION"
+	svn delete --force tags/$PLUGINVERSION/readme.txt
+	svn copy trunk/readme.txt tags/$PLUGINVERSION
+fi
 svn copy --quiet trunk/ tags/$PLUGINVERSION/
-# Remove assets and trunk directories from tag directory
-svn delete --force --quiet $SVNPATH/tags/$PLUGINVERSION/assets
+# Remove trunk directories from tag directory
 svn delete --force --quiet $SVNPATH/tags/$PLUGINVERSION/trunk
 svn update --quiet --accept working $SVNPATH/tags/$PLUGINVERSION
 #svn resolve --accept working $SVNPATH/tags/$PLUGINVERSION/*
